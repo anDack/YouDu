832088d8d8438c9e6c817fe9ae0fdf68abb79cbc
anDack <1160083806@qq.com>
2017-02-13 11:16:12 +0800

    第二个比较大型的项目

---
+++ app/src/main/java/com/andack/youdu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.andack.youdu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
